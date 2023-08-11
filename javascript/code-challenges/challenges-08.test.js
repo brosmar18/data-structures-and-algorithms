abc6abfa34845f785e6adb911291fde168ac1176
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-08-11 12:07:12 -0500

    4 updated

--- javascript/code-challenges/challenges-08.test.js
+++ javascript/code-challenges/challenges-08.test.js
@@ -103,7 +103,7 @@ Write a function named containsWorld that takes in a string or number of any len
 
 const containsWorld = (input) => {
   // Solution code here...
-  const regex = /\d/;
+  const regex = /world/;
   return regex.test(input.toString());
 };
 
@@ -129,6 +129,8 @@ Write a function named citiesAtoJ that takes in an array of city names and uses 
 
 const citiesAtoJ = (arr) => {
   // Solution code here...
+  const regex = /^[A-J]/;
+  return arr.filter(city => regex.test(city));
 };
 
 /* ------------------------------------------------------------------------------------------------

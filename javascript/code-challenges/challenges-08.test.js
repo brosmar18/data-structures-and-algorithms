ca205396abbcda8efdc2abc0bb503c35eb7f3b9a
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-08-11 12:04:01 -0500

    4 completed

--- javascript/code-challenges/challenges-08.test.js
+++ javascript/code-challenges/challenges-08.test.js
@@ -103,6 +103,8 @@ Write a function named containsWorld that takes in a string or number of any len
 
 const containsWorld = (input) => {
   // Solution code here...
+  const regex = /\d/;
+  return regex.test(input.toString());
 };
 
 /* ------------------------------------------------------------------------------------------------

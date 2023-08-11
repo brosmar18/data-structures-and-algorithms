66497c3912898f79008b6df7b4db1fb7e77920b6
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-08-11 12:13:06 -0500

    4 updated

--- javascript/code-challenges/challenges-09.test.js
+++ javascript/code-challenges/challenges-09.test.js
@@ -66,6 +66,7 @@ HR has asked you to change the data to make it easier to print so that it looks 
 
 const updateNumbers = (obj) => {
   // Solution code here...
+  return Object.entries(obj).map(([key, value]) => `${key}: ${value}`);
 };
 
 

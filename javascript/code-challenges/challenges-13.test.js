c1a794a7e46cbc9103aee25f74781cef9dc878ef
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-09-12 20:03:30 -0500

    challenge 5 completed

--- javascript/code-challenges/challenges-13.test.js
+++ javascript/code-challenges/challenges-13.test.js
@@ -96,6 +96,13 @@ For example, 'abcdefg' returns 'bdf'
 
 const onlyOddChars = (str) => {
   // Solution code here...
+  let result = '';
+
+  for (let i = 1; i < str.length; i += 2) {
+    result += str[i];
+  }
+
+  return result;
 };
 
 /* ------------------------------------------------------------------------------------------------

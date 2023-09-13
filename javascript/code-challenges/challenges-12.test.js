c0ccba33ccad9e64e6f2e1b1f0dbf393c5a6a5b8
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-09-12 19:55:18 -0500

    challenge 5 completed

--- javascript/code-challenges/challenges-12.test.js
+++ javascript/code-challenges/challenges-12.test.js
@@ -69,6 +69,7 @@ Note: if you ever need to validate an email using a regex in practice, the Inter
 
 const validateEmail = (email) => {
   // Solution code here...
+  return /^[a-zA-Z0-9]+(\.[a-zA-Z0-9]+)?@[a-zA-Z0-9]+(\.(com|net|org))$/.test(email);
 };
 
 /* ------------------------------------------------------------------------------------------------

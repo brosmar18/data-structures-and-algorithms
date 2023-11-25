b833c27f49f23e7f009fb4aaf09cb0b913d47890
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-11-25 11:41:53 -0600

    all tests passing

--- javascript/animalShelter/index.js
+++ javascript/animalShelter/index.js
@@ -9,28 +9,32 @@ class Animal {
 
 class AnimalShelter {
     constructor() {
-      this.dogs = [];
-      this.cats = [];
+        this.dogs = [];
+        this.cats = [];
     }
-  
+
     enqueue(animal) {
-      if (animal.species.toLowerCase() === 'dog') {
-        this.dogs.push(animal);
-      } else if (animal.species.toLowerCase() === 'cat') {
-        this.cats.push(animal);
-      }
+        if (animal.species.toLowerCase() === 'dog') {
+            this.dogs.push(animal);
+        } else if (animal.species.toLowerCase() === 'cat') {
+            this.cats.push(animal);
+        }
     }
-  
+
     dequeue(pref) {
-      let preference = pref.toLowerCase();
-      if (preference === 'dog' && this.dogs.length > 0) {
-        return this.dogs.shift();
-      } else if (preference === 'cat' && this.cats.length > 0) {
-        return this.cats.shift();
-      } else {
-        return null;
-      }
+        if (!pref) {
+            return null;
+        }
+
+        let preference = pref.toLowerCase();
+        if (preference === 'dog' && this.dogs.length > 0) {
+            return this.dogs.shift();
+        } else if (preference === 'cat' && this.cats.length > 0) {
+            return this.cats.shift();
+        } else {
+            return null;
+        }
     }
-  }
+}
 
 module.exports = { AnimalShelter, Animal }
